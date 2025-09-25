537468c0f25120f4e6d575e2a6be312c42daf2f5
Hestia-n <hestiasafitri14@gmail.com>
2025-09-25 09:21:57 +0700

    Menghitung Luas dan Keliling Segitiga

--- Segitiga14.java
+++ Segitiga14.java
@@ -4,16 +4,22 @@ public class Segitiga14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("=== Program Hitung Luas Segitiga ===");
+        System.out.println("=== Program Hitung Luas & Keliling Segitiga ===");
         System.out.print("Masukkan alas segitiga (cm): ");
         double alas = sc.nextDouble();
 
         System.out.print("Masukkan tinggi segitiga (cm): ");
         double tinggi = sc.nextDouble();
 
-        double luas = 0.5 * alas * tinggi;
+        System.out.print("Masukkan sisi miring segitiga (cm): ");
+        double sisiMiring = sc.nextDouble();
 
-        System.out.println("Luas segitiga = " + luas + " cm²");
+        double luas = 0.5 * alas * tinggi;
+        double keliling = alas + tinggi + sisiMiring;
+
+        System.out.println("\nHasil Perhitungan:");
+        System.out.println("Luas segitiga     = " + luas + " cm²");
+        System.out.println("Keliling segitiga = " + keliling + " cm");
 
         sc.close();
     }
